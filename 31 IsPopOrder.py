54e6eec0c4f7ce01993b9a26a5cb202e9c59004a
akivilib <linyw7@gmail.com>
2018-11-06 14:45:17 +0800

    Create 31 IsPopOrder.py

---
+++ 31 IsPopOrder.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def IsPopOrder(self, pushV, popV):
+        # write code here
+        if not pushV or pushV == []:
+            return False
+        stack = []
+        j = 0
+        for i in range(len(pushV)):
+            stack.append(pushV[i])
+            while j < len(popV) and stack and stack[-1] == popV[j]:
+                stack.pop()
+                j+=1
+        return (not stack)
